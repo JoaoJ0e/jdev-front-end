baa4bd0fff602b3aea05c3996af4621d0257d798
JoaoJ0e <joao.griebner@gmail.com>
2024-09-12 16:45:16 -0300

    Finalizado exercicio 10

--- aula-4/exercicio-10/script.js
+++ aula-4/exercicio-10/script.js
@@ -6,10 +6,15 @@ function addTarefa(event) {
         const novaTarefa = document.createElement('li');
         novaTarefa.classList.add('tarefa')
         novaTarefa.innerHTML =
-        // todo: trocar para duas divs para centralizar
-            `<p>${descTarefa.value}</p>
-            <button>Concluir</button>
-            <button>Deletar</button>`;
+            `
+            <div class="tarefa-buttons">
+            <button onclick="concluirTarefa(event)">Concluir</button>
+            <button onclick="deletarTarefa(event)">Deletar</button>
+            </div>
+            <div class="tarefa-desc">
+            <p>${descTarefa.value}</p>
+            </div>
+            `;
         listaTarefas.appendChild(novaTarefa);
 
     } else {
@@ -21,6 +26,44 @@ function addTarefa(event) {
         const textoTemp = document.getElementById('noTarefaText')
         listaTarefas.removeChild(textoTemp);
     }
-
+    atualizaContador();
     event.preventDefault();
+}
+
+function concluirTarefa(event) {
+    const tarefa = event.target.parentNode.parentNode;
+    tarefa.classList.add("tarefa-completa");
+    atualizaContador();
+}
+
+function deletaTarefasConcluidas(event) {
+
+    const listaTarefas = document.getElementById('lista-tarefas');
+    const tarefasCompletas = Array.from(document.getElementsByClassName('tarefa-completa'));
+
+    tarefasCompletas.forEach(tarefa => 
+        listaTarefas.removeChild(tarefa)
+    );
+
+    atualizaContador();
+    event.preventDefault();
+}
+
+function deletarTarefa(event) {
+    const tarefa = event.target.parentNode.parentNode;
+    tarefa.parentNode.removeChild(tarefa);
+    atualizaContador();
+}
+
+function atualizaContador() {
+    let totalTarefas = Array.from(document.getElementById('lista-tarefas').children).length;
+    const contadorPendentes = document.getElementById('qtdPendentes');
+    const contadorConcluidas = document.getElementById('qtdConcluidas');
+    const qtdCompletas = document.getElementsByClassName('tarefa-completa').length;
+
+    contadorConcluidas.textContent = qtdCompletas;
+    contadorPendentes.textContent = totalTarefas - qtdCompletas;
+
+    
+
 }
