e322b10d82eecc226eee2e1da27f67f27b93e26f
João Griebner <joao.griebner@gmail.com>
2024-09-18 18:44:42 -0300

    Upd exercicio 18/09

--- semana-2-angular/gerenciamento-usuario-produto/src/app/app.module.ts
+++ semana-2-angular/gerenciamento-usuario-produto/src/app/app.module.ts
@@ -6,10 +6,12 @@ import { AppComponent } from './app.component';
 import { ProdutoModule } from './main/produto/produto.module';
 import { UsuarioModule } from './main/usuario/usuario.module';
 import { CadastroModule } from "./main/produto/features/cadastro/cadastro.module";
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
--- semana-2-angular/gerenciamento-usuario-produto/src/app/main/usuario/services/usuario.service.ts
+++ semana-2-angular/gerenciamento-usuario-produto/src/app/main/usuario/services/usuario.service.ts
@@ -1,9 +1,13 @@
 import { Injectable } from '@angular/core';
+import { Usuario } from '../../../models/usuario';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UsuarioService {
+  private id = 0;
+  private usuarios: Array<Usuario> = [];
+
 
   constructor() { }
 }
