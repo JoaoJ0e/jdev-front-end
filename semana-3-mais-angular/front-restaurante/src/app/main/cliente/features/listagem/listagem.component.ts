a98f635d83c87031b5fdf4deb40051f614c09417
João Griebner <joao.griebner@gmail.com>
2024-09-26 20:06:23 -0300

    Add listagem (todo: cors setup)

--- semana-3-mais-angular/front-restaurante/src/app/main/cliente/features/listagem/listagem.component.ts
+++ semana-3-mais-angular/front-restaurante/src/app/main/cliente/features/listagem/listagem.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ClienteService } from '../../services/cliente.service';
+import { Cliente } from '../../../models/Cliente';
 
 @Component({
   selector: 'app-listagem',
   templateUrl: './listagem.component.html',
   styleUrl: './listagem.component.scss'
 })
-export class ListagemComponent {
+export class ListagemComponent implements OnInit{
+
+  clientes: Cliente[] = [];
+
+  constructor(
+    private clienteService: ClienteService
+  ) {}
+  
+  ngOnInit(): void {
+    this.clienteService.getClientes(1).subscribe((clientes) => this.clientes = clientes);
+  }
+
+
+  deleteCliente(arg0: any) {
+  throw new Error('Method not implemented.');
+  }
+  editCliente(_t17: any) {
+  throw new Error('Method not implemented.');
+  }
 
 }
--- semana-3-mais-angular/front-restaurante/src/app/main/cliente/services/cliente.service.ts
+++ semana-3-mais-angular/front-restaurante/src/app/main/cliente/services/cliente.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Cliente } from '../../models/Cliente';
 import { HttpClient } from '@angular/common/http';
-import { take } from 'rxjs';
+import { Observable, take } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -11,12 +11,14 @@ export class ClienteService {
     private http: HttpClient
    ) { }
 
-   getClientes(restauranteId: number) {
-    return this.http.get(`localhost:8080/cliente/restaurante/${restauranteId}`)
+   getClientes(restauranteId: number): Observable<Cliente[]> {
+    return this.http.get<Cliente[]>(`localhost:8080/cliente/restaurante/${restauranteId}`)
    }
 
    addCliente(cliente: Cliente) {
-     return this.http.post('localhost:8080/cliente', cliente);
+
+    console.log('post requested:')
+    return this.http.post('localhost:8080/cliente', cliente);
    }
 
   //  findById(id:number): Cliente | undefined {
